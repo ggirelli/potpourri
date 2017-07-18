45ed77d846f6834f1d91876388873d1a424f8a42
Gabriele Girelli <gigi.ga90@gmail.com>
2017-07-18 11:16:15 +0200

    Removing header first characters.

--- cosmic-census-fish-oligos/split_fa_by_gene.sh
+++ cosmic-census-fish-oligos/split_fa_by_gene.sh
@@ -15,7 +15,7 @@ mkdir -p $outdir
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
 	if [ ${line:0:1} == '>' ]; then
-    	name=$(echo "$line" | tr -d '>' | tr '_' ' ' | cut -d ' ' -f 1)
+    	name=$(echo "$line" | sed 's/^>\ //' | tr '_' ' ' | cut -d ' ' -f 1)
     	echo $line | tr ' ' '\n' >> $outdir"/"$name".fa"
     fi
 done < <(cat "$1" | paste - -)

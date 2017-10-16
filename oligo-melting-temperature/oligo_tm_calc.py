12d22aefec583d128467941d0d0187a9893a769e
Gabriele Girelli <gigi.ga90@gmail.com>
2017-10-16 15:27:41 +0200

    Added DNA/RNA and RNA/DNA duplex Tm calculation and sequence check.

--- oligo-melting-temperature/oligo_tm_calc.py
+++ oligo-melting-temperature/oligo_tm_calc.py
@@ -43,10 +43,11 @@ Calculate melting temeprature of a DNA duplex at provided [oligo],
 [Na+], [Mg2+]. Either provide an oligo sequence or a file with one oligo
 per line (and use -F option). References:
  [1] Freier et al, PNAS(83), 1986;
- [2] Allawi & Santalucia, Biochemistry(36), 1997;
- [3] SantaLucia, PNAS(95), 1998;
- [4] Owczarzy et al, Biochemistry(43), 2004;
- [5] Owczarzy et al, Biochemistry(47), 2008.
+ [2] Sugimoto et al, Biochemistry(34), 1995.
+ [3] Allawi & Santalucia, Biochemistry(36), 1997;
+ [4] SantaLucia, PNAS(95), 1998;
+ [5] Owczarzy et al, Biochemistry(43), 2004;
+ [6] Owczarzy et al, Biochemistry(47), 2008;
 ''')
 
 # Add mandatory arguments
@@ -72,9 +73,11 @@ parser.add_argument('-m', '--mgconc', metavar = "mg_conc",
 	default = [0])
 parser.add_argument('-t', '--thermo', type = str, nargs = 1,
 	help = '''Thermodynamic table to use in the calculations.
-	Possible values: allawi (based on ref.2, default) or 
-	freier (based on ref.1).''',
-	choices = ['allawi', 'freier'], default = ['allawi'])
+	Possible values: allawi (based on ref.3, default), freier (based on ref.1),
+	sugimotod (based on ref.2., given DNA sequence) or
+	sugimotor (based on ref2., given RNA sequence).''',
+	choices = ['allawi', 'freier', 'sugimotor', 'sugimotod'],
+	default = ['allawi'])
 
 # Add flags
 parser.add_argument('-C', '--celsius',
@@ -130,22 +133,16 @@ freier = {
 	#				 dH0	 dS0 	 dG0
 	'AA'		:	(-6.6,	-18.4,	-0.9),
 	'UU'		:	(-6.6,	-18.4,	-0.9),
-	'TT'		:	(-6.6,	-18.4,	-0.9),
 	'AU'		:	(-5.7,	-15.5,	-0.9),
-	'AT'		:	(-5.7,	-15.5,	-0.9),
 	'UA'		:	(-8.1,	-22.6,	-1.1),
-	'TA'		:	(-8.1,	-22.6,	-1.1),
 	'CA'		:	(-10.5,	-27.8,	-1.8),
 	'UG'		:	(-10.5,	-27.8,	-1.8),
 	'TG'		:	(-10.5,	-27.8,	-1.8),
 	'CU'		:	(-7.6,	-19.2,	-1.7),
-	'CT'		:	(-7.6,	-19.2,	-1.7),
 	'AG'		:	(-7.6,	-19.2,	-1.7),
 	'GA'		:	(-13.3,	-35.5,	-2.3),
 	'UC'		:	(-13.3,	-35.5,	-2.3),
-	'TC'		:	(-13.3,	-35.5,	-2.3),
 	'GU'		:	(-10.2,	-26.2,	-2.1),
-	'GT'		:	(-10.2,	-26.2,	-2.1),
 	'AC'		:	(-10.2,	-26.2,	-2.1),
 	'CG'		:	(-8.0,	-19.4,	-2.0),
 	'GC'		:	(-14.2,	-34.9,	-3.4),
@@ -153,12 +150,69 @@ freier = {
 	'CC'		:	(-12.2,	-29.7,	-2.9),
 	'endA'		:	(0,		-10.8,	3.4),
 	'endC'		:	(0,		-10.8,	3.4),
-	'endT'		:	(0,		-10.8,	3.4),
 	'endG'		:	(0,		-10.8,	3.4),
 	'endU'		:	(0,		-10.8,	3.4),
 	'has_end'	:	True,
 	'sym'		:	(0,		-1.4,	0.4),
 	'has_sym'	:	True
+}
+
+# Table from Sugimoto et al, Biochemistry(34), 1995 - in 1 M NaCl [DNA/RNA duplex]
+# For calculation from the RNA sequence 5'-to-3'
+# dH0: kcal / mol
+# dS0: cal / (K mol)
+# dG0: kcal / mol
+sugimotor = {
+	#				dH0		dS0		dG0
+	'AA'		:	(-7.8,	-21.9,	-1.0),
+	'AC'		:	(-5.9,	-12.3,	-2.1),
+	'AG'		:	(-9.1,	-23.5,	-1.8),
+	'AU'		:	(-8.3,	-23.9,	-0.9),
+	'CA'		:	(-9.0,	-26.1,	-0.9),
+	'CC'		:	(-9.3,	-23.2,	-2.1),
+	'CG'		:	(-16.3,	-47.1,	-1.7),
+	'CU'		:	(-7.0,	-19.7,	-0.9),
+	'GA'		:	(-5.5,	-13.5,	-1.3),
+	'GC'		:	(-8.0,	-17.1,	-2.7),
+	'GG'		:	(-12.8,	-31.9,	-2.9),
+	'GU'		:	(-7.8,	-21.6,	-1.1),
+	'UA'		:	(-7.8,	-23.2,	-0.6),
+	'UC'		:	(-8.6,	-22.9,	-1.5),
+	'UG'		:	(-10.4,	-28.4,	-1.6),
+	'UU'		:	(-11.5,	-36.4,	-0.2),
+	'init'		:	(1.9,	-3.9,	3.1),
+	'has_init'	:	True,
+	'has_end'	:	False,
+	'has_sym'	:	False
+}
+
+# Table from Sugimoto et al, Biochemistry(34), 1995 - in 1 M NaCl [DNA/RNA duplex]
+# For calculation from the DNA sequence 5'-to-3'
+# dH0: kcal / mol
+# dS0: cal / (K mol)
+# dG0: kcal / mol
+sugimotod = {
+	#				dH0		dS0		dG0
+	'TT'		:	(-7.8,	-21.9,	-1.0),
+	'GT'		:	(-5.9,	-12.3,	-2.1),
+	'CT'		:	(-9.1,	-23.5,	-1.8),
+	'AT'		:	(-8.3,	-23.9,	-0.9),
+	'TG'		:	(-9.0,	-26.1,	-0.9),
+	'GG'		:	(-9.3,	-23.2,	-2.1),
+	'CG'		:	(-16.3,	-47.1,	-1.7),
+	'AG'		:	(-7.0,	-19.7,	-0.9),
+	'TC'		:	(-5.5,	-13.5,	-1.3),
+	'GC'		:	(-8.0,	-17.1,	-2.7),
+	'CC'		:	(-12.8,	-31.9,	-2.9),
+	'AC'		:	(-7.8,	-21.6,	-1.1),
+	'TA'		:	(-7.8,	-23.2,	-0.6),
+	'AG'		:	(-8.6,	-22.9,	-1.5),
+	'CA'		:	(-10.4,	-28.4,	-1.6),
+	'AA'		:	(-11.5,	-36.4,	-0.2),
+	'init'		:	(1.9,	-3.9,	3.1),
+	'has_init'	:	True,
+	'has_end'	:	False,
+	'has_sym'	:	False
 }
 
 # Table from Allawi&Santalucia, Biochemistry(36), 1997 - in 1 M NaCl [DNA]
@@ -195,7 +249,9 @@ allawi = {
 # Save tables
 tt = {
 	'freier' 	: freier,
-	'allawi' 	: allawi
+	'allawi' 	: allawi,
+	'sugimotor'	: sugimotor,
+	'sugimotod'	: sugimotod
 }
 
 # FUNCTIONS ====================================================================
@@ -261,6 +317,21 @@ def calc(seq, oligo_conc, na_conc, mg_conc, tt, tt_mode, celsius, is_verbose):
 	# Make string uppercase
 	seq = seq.upper()
 
+	# Check that all characters code for bases
+	if not 0 == sum([1 for b in seq if b not in 'TCGAU']):
+		print('The provided sequence contains non-nucleic acid characters.')
+		return
+
+	# Check that the correct nucleic acid type was provided
+	if tt_mode in ['sugimotod', 'allawi']:
+		if 'U' in seq:
+			print('The option -t %s requires a DNA sequence.' % (tt_mode,))
+			return
+	elif tt_mode in ['sugimotor', 'freier']:
+		if 'T' in seq:
+			print('The option -t %s requires a RNA sequence.' % (tt_mode,))
+			return
+
 	# Make NN couples
 	couples = [seq[i:(i+2)] for i in range(len(seq) - 1)]
 
@@ -279,6 +350,8 @@ def calc(seq, oligo_conc, na_conc, mg_conc, tt, tt_mode, celsius, is_verbose):
 	if tt[tt_mode]['has_end']:
 		h += tt[tt_mode]['end%s' % (seq[0],)][0]
 		h += tt[tt_mode]['end%s' % (seq[-1],)][0]
+	if tt[tt_mode]['has_init']:
+		h += tt[tt_mode]['init'][0]
 
 	# Correct enthalpy for symmetry
 	if tt[tt_mode]['has_sym'] and seq == rc(seq, 'dna'):
@@ -287,10 +360,12 @@ def calc(seq, oligo_conc, na_conc, mg_conc, tt, tt_mode, celsius, is_verbose):
 	# Calculate dS0(N-N) in kcal / (K mol)
 	s = sum([tt[tt_mode][c][1] for c in couples])
 
-	# Add initiation enthalpy
+	# Add initiation enthropy
 	if tt[tt_mode]['has_end']:
 		s += tt[tt_mode]['end%s' % (seq[0],)][1]
 		s += tt[tt_mode]['end%s' % (seq[-1],)][1]
+	if tt[tt_mode]['has_init']:
+		s += tt[tt_mode]['init'][1]
 
 	# Correct enthalpy for symmetry
 	if tt[tt_mode]['has_sym'] and seq == rc(seq, 'dna'):
@@ -299,7 +374,10 @@ def calc(seq, oligo_conc, na_conc, mg_conc, tt, tt_mode, celsius, is_verbose):
 	s /= 1e3
 
 	# Calculate melting temperature in Celsius
-	Tm1 = h / (s + R * math.log(oligo_conc))
+	if 'sugimoto' in tt_mode:
+		Tm1 = h / (s + R * math.log(oligo_conc / 4))
+	else:
+		Tm1 = h / (s + R * math.log(oligo_conc))
 
 	# Adjusted for [Na]
 	# Based on Owczarzy et al, Biochemistry(43), 2004
